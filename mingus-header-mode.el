0fd8c319b0a668759b3807118161e34fa0d49e66
Aaron Jacobs <atheriel@gmail.com>
2017-01-08 16:48:23 -0500

    Initial commit. Contains an elisp project skeleton.
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

---
+++ mingus-header-mode.el
@@ -0,0 +1,39 @@
+;;; mingus-header-mode.el --- A minor mode for displaying MPD information in the header-line -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017 Aaron Jacobs
+
+;; This file is NOT part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(require 'mingus)
+(require 'powerline)
+
+;;;; Customization
+
+(defgroup mingus-header nil
+  "Customizations for `mingus-header-mode'."
+  :group 'mingus)
+
+(provide 'mingus-header-mode)
+
+;; Local Variables:
+;; coding: utf-8
+;; End:
+
+;;; mingus-header-mode.el ends here
